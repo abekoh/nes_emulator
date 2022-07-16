fc040a172eaf84a38dea42c6a42017219c763583
Kotaro Abe <abekoh.bcky@gmail.com>
2022-07-16 09:57:30 +0900

    refactor: remove unused

--- src/lib.rs
+++ src/lib.rs
@@ -95,8 +95,6 @@ impl CPU {
                     self.lda(&AddressingMode::Absolute);
                     self.program_counter += 2;
                 }
-                0xAA => self.tax(),
-                0xE8 => self.inx(),
                 0x00 => return,
                 _ => todo!()
             }
@@ -161,18 +159,6 @@ impl CPU {
         self.update_zero_and_negative_flags(self.register_a);
     }
 
-    #[deprecated]
-    fn tax(&mut self) {
-        self.register_x = self.register_a;
-        self.update_zero_and_negative_flags(self.register_x);
-    }
-
-    #[deprecated]
-    fn inx(&mut self) {
-        self.register_x = self.register_x.overflowing_add(1).0;
-        self.update_zero_and_negative_flags(self.register_x);
-    }
-
     fn update_zero_and_negative_flags(&mut self, result: u8) {
         if result == 0 {
             self.status = self.status | 0b0000_0010;
@@ -186,33 +172,6 @@ impl CPU {
             self.status = self.status & 0b0111_1111;
         }
     }
-
-    #[deprecated]
-    pub fn interpret(&mut self, program: Vec<u8>) {
-        self.program_counter = 0;
-        loop {
-            let opscode = program[self.program_counter as usize];
-            self.program_counter += 1;
-            match opscode {
-                0xA9 => {
-                    self.lda(&AddressingMode::Immediate);
-                    self.program_counter += 1;
-                }
-                0xA5 => {
-                    self.lda(&AddressingMode::ZeroPage);
-                    self.program_counter += 1;
-                }
-                0xAD => {
-                    self.lda(&AddressingMode::Absolute);
-                    self.program_counter += 2;
-                }
-                0xAA => self.tax(),
-                0xE8 => self.inx(),
-                0x00 => return,
-                _ => todo!()
-            }
-        }
-    }
 }
 
 #[cfg(test)]
@@ -220,51 +179,10 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_0xa9_lda_immediate_load_data() {
-        let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x05, 0x00]);
-        assert_eq!(cpu.register_a, 0x05);
-        assert_eq!(cpu.status & 0b0000_0010, 0);
-        assert_eq!(cpu.status & 0b1000_0000, 0);
-    }
-
-    #[test]
     fn test_lda_from_memory() {
         let mut cpu = CPU::new();
         cpu.mem_write(0x10, 0x55);
         cpu.load_and_run(vec![0xa5, 0x10, 0x00]);
         assert_eq!(cpu.register_a, 0x55);
-    }
-
-    #[test]
-    fn test_0xa9_lda_zero_flag() {
-        let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x00, 0x00]);
-        assert_eq!(cpu.status & 0b0000_0010, 0b10);
-    }
-
-    #[test]
-    fn test_0xaa_tax_move_a_to_x() {
-        let mut cpu = CPU::new();
-        cpu.register_a = 10;
-        cpu.interpret(vec![0xaa, 0x00]);
-        assert_eq!(cpu.register_x, 10);
-    }
-
-    #[test]
-    fn test_5_ops_working_together() {
-        let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
-
-        assert_eq!(cpu.register_x, 0xc1);
-    }
-
-    #[test]
-    fn test_inx_overflow() {
-        let mut cpu = CPU::new();
-        cpu.register_x = 0xff;
-        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
-
-        assert_eq!(cpu.register_x, 1)
     }
 }
