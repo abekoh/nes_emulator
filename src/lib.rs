6039f13dcd183b55dc0bcb87d206a0b6021cc157
Kotaro Abe <abekoh.bcky@gmail.com>
2022-07-16 09:20:44 +0900

    feat: AddressingMode enum

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,21 @@ pub struct CPU {
     memory: [u8; 0xFFFF],
 }
 
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
+pub enum AddressingMode {
+    Immediate,
+    ZeroPage,
+    ZeroPage_X,
+    ZeroPage_Y,
+    Absolute,
+    Absolute_X,
+    Absolute_Y,
+    Indirect_X,
+    Indirect_Y,
+    NoneAddressing,
+}
+
 impl CPU {
     pub fn new() -> Self {
         CPU {
