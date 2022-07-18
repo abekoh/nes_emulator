e99a1bea0409b391906a586f99b0b6347f047b77
Kotaro Abe <abekoh.bcky@gmail.com>
2022-07-18 11:52:56 +0900

    fix

--- src/cpu.rs
+++ src/cpu.rs
@@ -163,7 +163,7 @@ impl CPU {
         self.a = 0;
         self.x = 0;
         self.status = 0;
-        self.pc = self.mem_read_u16(0xFFFC);
+        self.pc = self.mem_read_u16(PROGRAM_BEGIN);
     }
 
     pub fn run(&mut self) {
