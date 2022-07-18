fcf98bdda8835a608dd607c0b549820ed4638833
Kotaro Abe <abekoh.bcky@gmail.com>
2022-07-18 11:19:09 +0900

    tmp: snake game

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,22 @@ fn main() {
 
     let mut cpu = CPU::new();
     cpu.load_reset(game_code);
-    cpu.run_with_callback(move |cpu| {});
+
+    let mut screen_state = [0 as u8; 32 * 3 * 32];
+    let mut rng = rand::thread_rng();
+
+    cpu.run_with_callback(move |cpu| {
+        handle_user_input(cpu, &mut event_pump);
+        cpu.mem_write(0xfe, rng.get_range(1, 16));
+
+        if read_screen_state(cpu, &mut screen_state) {
+            texture.update(None, &screen_state, 32 * 3).unwrap();
+            canvas.copy(&texture, None, None).unwrap();
+            canvas.present();
+        }
+
+        ::std::thread::sleep(std::time::Duration::new(0, 70_000));
+    });
 }
 
 fn handle_user_input(cpu: &mut CPU, event_pump: &mut EventPump) {
